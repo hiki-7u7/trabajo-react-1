c9d8149fe0d9d23164355cbb20372c32b8aa2949
Nini <raychu701@gmail.com>
2023-11-19 20:48:00 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import {BrowserRounter,routes,route} from "react-router-dom";
+
+import {NavBar} from "./components/navbar";
+import {ItemListContainer} from "./components/ItemListContainer";
+import {Error404} from "./components/Error404";
+
+function App() {
+  return (
+    <BrowserRounter>
+    <NavBar/>
+    <Routes>
+        <Route path="/" element={<ItemListContainer greeting="productos"/>}/>
+        <Route path="/category/:id" element={<ItemListContainer greeting="productos"/>}/>
+        <Route path="/items/:id" element={ItemListContainer}/>
+        <Route path="*"element={<Error404/>}/>
+    </Routes>
+  </BrowserRounter>
+  )
+}
+
+export default App
---
+++ src/components/Error404.jsx
@@ -0,0 +1,8 @@
+import container from "react-bootstrap/container";
+
+export const Error404=() =>{
+return 
+    <Container className="mt-4">
+        <h1>Recurso no encontrado</h1>
+    </Container>
+};
